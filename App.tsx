81b1593b0eb6411d7ca898e0470fd31eb5e72dbf
andrew-matt <97734646+andrew-matt@users.noreply.github.com>
2022-02-28 17:26:20 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import Accordion from "./components/Accordion/Accordion";
-import {OnOff} from './components/OnOff/OnOff';
-import {Rating} from "./components/Rating/Rating";
-import s from './components/OnOff/OnOff.module.css';
+import {Rating, RatingValueType} from "./components/Rating/Rating";
 import {UncontrolledAccordion} from "./components/UncontrolledAccordion/UncontrolledAccordion";
 import {UncontrolledRating} from "./components/UncontrolledRating/UncontrolledRating";
+import Accordion from "./components/Accordion/Accordion";
+import {UncontrolledOnOff} from "./components/UncontrolledOnOff/UncontrolledOnOff";
+import {OnOff} from "./components/OnOff/OnOff";
 
 function sum(a: number, b: number) {
     alert(a + b)
@@ -15,10 +15,16 @@ function sum(a: number, b: number) {
 sum(100, 300);*/
 
 function App() {
+
+    let [ratingValue, setRatingValue] = useState<RatingValueType>(0)
+    let [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false)
+    let [on, setOn] = useState<boolean>(false)
+
     console.log("App rendering")
     return (
         <div className={"App"}>
-            {/*<OnOff/>*/}
+            <UncontrolledOnOff/>
+            <OnOff on={on} onClick={() => setOn(!on)}/>
             {/*  <img
                 src={"https://i.ytimg.com/vi/QIBNa-AIw8g/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLAlxxFzxMagdsK2HGpW_HmTO71gCg"}/>
             <input/>
@@ -28,23 +34,17 @@ function App() {
             <PageTitle title={"This is APP component"}/>
             <PageTitle title={"My friends"}/>
             Article 1
-            <Rating value={3}/>
-            <Accordion titleValue={"Menu"} collapsed={true}/>
-            <Accordion titleValue={"Users"} collapsed={false}/>*/}
+            <Rating value={3}/>*/}
+            <Accordion titleValue={"Menu"} collapsed={accordionCollapsed} onClick={() => setAccordionCollapsed(!accordionCollapsed)}/>
+             {/* <Accordion titleValue={"Users"} collapsed={false}/>*/}
             <UncontrolledAccordion titleValue={"Menu"}/>
-            <UncontrolledAccordion titleValue={"Users"}/>
             {/* Article 2
             <Rating value={0}/>
             <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
+            <Rating value={2}/>*/}
+            <Rating value={ratingValue} onClick={setRatingValue}/>
+            {/*<Rating value={4}/>
             <Rating value={5}/>*/}
-            <UncontrolledRating/>
-            <UncontrolledRating/>
-            <UncontrolledRating/>
-            <UncontrolledRating/>
-            <UncontrolledRating/>
             <UncontrolledRating/>
         </div>
     );
