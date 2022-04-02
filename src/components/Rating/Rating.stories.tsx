ada313575f751db40dde79fc2e974bd02b9a1f1d
andrew-matt <andrew.matt@ya.ru>
2022-04-02 15:47:15 +0300

    update RatingStories

--- src/components/Rating/Rating.stories.tsx
+++ src/components/Rating/Rating.stories.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Rating} from "./Rating";
+import React, {useState} from 'react';
+import {Rating, RatingValueType} from "./Rating";
 
 export default {
     title: 'Rating stories',
@@ -12,3 +12,7 @@ export const Rating2 = () => <Rating value={2} onClick={a => a}/>;
 export const Rating3 = () => <Rating value={3} onClick={a => a}/>;
 export const Rating4 = () => <Rating value={4} onClick={a => a}/>;
 export const Rating5 = () => <Rating value={5} onClick={a => a}/>;
+export const RatingChanging = () => {
+    const [rating, setRating] = useState<RatingValueType>(3)
+    return <Rating value={rating} onClick={setRating}/>
+}
