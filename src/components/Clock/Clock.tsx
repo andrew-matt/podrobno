c6c9cb2b74a2177395a7243cd26cbd980eae0e41
andrew-matt <andrew.matt@ya.ru>
2022-05-27 10:57:22 +0300

    add getTime func

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,23 +1,30 @@
 import React, {useEffect, useState} from "react";
 
+const getTime = (time: number) => time < 10 ? "0" + time : time
+
 export const Clock = () => {
     const [date, setDate] = useState(new Date);
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-    const time = `${hours < 10 ? "0" + hours : hours}:${minutes < 10 ? "0" + minutes : minutes}:${seconds < 10 ? "0" + seconds : seconds}`;
 
     useEffect(() => {
-        setInterval(() => {
+        const intervalID = setInterval(() => {
             console.log("TICK")
             setDate(new Date);
         }, 1000)
+
+        return () => {
+            clearInterval(intervalID)
+        }
+
     }, [])
 
     return (
-        <>
-            {time}
-        </>
+        <div>
+            <span>{getTime(date.getHours())}</span>
+            :
+            <span>{getTime(date.getMinutes())}</span>
+            :
+            <span>{getTime(date.getSeconds())}</span>
+        </div>
     )
 }
 
